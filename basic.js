fd5280ad2a5a1cbd927c603a18443f4aea59adcf
Arafath987 <94624401+Arafath987@users.noreply.github.com>
2026-02-25 10:02:49 +0530

    git basic variable added

--- basic.js
+++ basic.js
@@ -1,3 +1,11 @@
 var l=10
 var l=20
-console.log(l)
+console.log(l)//this not an error
+
+let m=10
+//let m=20 //this is an error because we can't declare a variable with the same name using let
+console.log(m)
+
+const n=10
+//n=20 //this is an error because we can't reassign a value to a constant variable
+console.log(n)
